739faf9f2fd32a540f51340a520b481632e1a326
nicoconti1901 <nico.conti.1901@gmail.com>
2024-01-02 11:49:49 -0300

    archivos php de admin para CRUD de propiedades terminado

---
+++ admin/index.php
@@ -0,0 +1,12 @@
+<?php
+require '../includes/funciones.php';
+incluirTemplate('header');
+?>
+<main class="contenedor seccion">
+      <h1>ADMINISTRADOR DE BIENES Y RAICES</h1>
+      <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva propeidad</a>
+</main>
+
+<?php
+incluirTemplate('footer')
+?>
