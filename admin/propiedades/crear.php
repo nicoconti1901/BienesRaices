377224473540a89634988f29a1707ebed4438484
nicoconti1901 <nico.conti.1901@gmail.com>
2024-01-02 13:58:22 -0300

    formulario de crear propiedad terminado sin conexion a BD

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -3,8 +3,48 @@ require '../../includes/funciones.php';
 incluirTemplate('header');
 ?>
 <main class="contenedor seccion">
-      <h1>CREAR</h1>
-      <a href="/admin" class="boton boton-verde">Volver</a>
+    <h1>CREAR</h1>
+    <a href="/admin" class="boton boton-verde">Volver</a>
+
+    <form class="formulario">
+        <fieldset>
+            <legend>Informacion General</legend>
+
+            <label for="titulo">Titulo: </label>
+            <input type="text" id="titulo" placeholder="Titulo Propiedad">
+
+            <label for="precio">Precio: </label>
+            <input type="number" id="precio" placeholder="Precio Propiedad">
+
+            <label for="imagen">Imagen: </label>
+            <input type="file" id="imagen" accept="image/jpeg, image/png">
+
+            <label for="descripcion">Descripcion: </label>
+            <textarea id="descripcion"></textarea>
+        </fieldset>
+
+        <fieldset>
+            <legend>Informacion Propiedad</legend>
+
+            <label for="habitaciones">Habitaciones: </label>
+            <input type="number" id="habitaciones" placeholder="Ej: 3" min="1" max="9">
+
+            <label for="wc">Baños: </label>
+            <input type="number" id="wc" placeholder="Ej: 3" min="1" max="9">
+
+            <label for="estacionamiento">Estacionamiento: </label>
+            <input type="number" id="estacionamiento" placeholder="Ej: 3" min="1" max="9">
+        </fieldset>
+        <fieldset>
+            <legend>Vendedor</legend>
+
+            <select>
+                <option value="1">Vendedor 1</option>
+                <option value="2">Vendedor 2</option>
+            </select>
+        </fieldset>
+        <input type="submit" value="Crear Propiedad" class="boton boton-verde">
+    </form>
 </main>
 
 <?php
